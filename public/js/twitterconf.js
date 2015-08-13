57564a01958b1ac5d27635a18dc26651b452c5fa
Nathan Isom <nathanisom27@gmail.com>
2015-08-12 22:49:14 -0500

    Only color changes left.

---
+++ public/js/twitterconf.js
@@ -0,0 +1,13 @@
+
+
+
+var config1 = {
+  "id": '480798133039816704',
+  "domId": 'tweets',
+  "maxTweets": 4,
+  "enableLinks": true,
+  "showUser": true,
+  "showTime": false,
+  "showInteraction": false
+};
+twitterFetcher.fetch(config1);
